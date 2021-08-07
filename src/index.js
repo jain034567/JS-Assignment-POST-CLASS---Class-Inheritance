427f704eb56af18fb4493fcbe7f8ce0b675b9ee4
jain034567 <87779765+jain034567@users.noreply.github.com>
2021-08-07 18:40:47 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import Circle from "./components/circle.js";
+class ShapeCircle extends Component {
+  render(){
+  return (<div>
+          <Circle color = "blue" />
+          </div>);
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
