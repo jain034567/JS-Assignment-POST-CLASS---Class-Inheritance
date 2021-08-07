ff7930502a91f65e76fad4cb93e406a5a2645b67
jain034567 <87779765+jain034567@users.noreply.github.com>
2021-08-07 17:29:11 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,20 @@
+import { component } from ./react;
 // declare class
-
+class Shape exptends Component{
+constructor(props){
+super(props);
+  function drawShape(){
+  }
+  function calculateArea(){
+  }
+  this.state = {color: "red"};
+}
+  render(){
+  return(<>
+         <h2> I am a color {this.state.color} </h2>
+        </>
+        );
+  }
+}
 // export class using module.exports
+export default Shape;
