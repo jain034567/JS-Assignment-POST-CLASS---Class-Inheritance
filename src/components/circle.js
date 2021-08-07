5d9e25bcff85cc67b5707007ba74469e02f06978
jain034567 <87779765+jain034567@users.noreply.github.com>
2021-08-07 17:34:19 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,11 @@
 // import using require
-
+import Shape from './shape.js';
 // declare class
-
-
+class Circle extends Component{
+render(){
+  return (<Shape color = {color} />
+);
+}
+}
+export default circle;
 // export class using module.exports
