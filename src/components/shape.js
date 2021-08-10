7d0a68d93a220c63e27cc361d072752c33672a60
jain034567 <87779765+jain034567@users.noreply.github.com>
2021-08-10 21:38:46 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,20 +1,32 @@
-import { component } from 'react';
-// declare class
-class Shape extends Component{
-    constructor(){
-        super();
-        this.state={color:"#000000"};
-    }
-    drawShape=()=>{ return shape = 0;};
-    calculateArea=()=>{let area =2*4;
-    return area;};
-    render(){
-        return(<>
-        {this.state.color} 
-        {this.drawShape} 
-        {this.calculateArea}
-        </>);
-    }
+// import { component } from 'react';
+// // declare class
+// class Shape extends Component{
+//     constructor(){
+//         super();
+//         this.state={color:"#000000"};
+//     }
+//     drawShape=()=>{ return shape = 0;};
+//     calculateArea=()=>{let area =2*4;
+//     return area;};
+//     render(){
+//         return(<>
+//         {this.state.color} 
+//         {this.drawShape} 
+//         {this.calculateArea}
+//         </>);
+//     }
+// }
+// // export class using module.exports
+// module.exports = Shape;
+module.exports = class Shape {
+    constructor(c){
+        this.color=c===undefined?"red":c;
+        }
+        drawShape(){
+            console.log("draw the shape"); 
+        }
+        calculateArea(){
+           console.log("area is area"); 
+           return 1;
+        }
 }
-// export class using module.exports
-module.exports = Shape;
