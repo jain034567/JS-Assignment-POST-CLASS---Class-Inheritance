4753e472d6355dc1d94db13e1bbf38688287e791
jain034567 <87779765+jain034567@users.noreply.github.com>
2021-08-10 21:41:02 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import Circle from "./components/circle.js";
-import Shape from "./components/shape.js";
-class ShapeCircle extends Component {
-  render(){
-  return <Circle />;
-  }
-}
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
