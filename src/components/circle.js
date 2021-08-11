6d0ff98aae6d4eceabfe1e51decd2b10dd47df42
jain034567 <87779765+jain034567@users.noreply.github.com>
2021-08-11 11:01:59 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,30 +1,30 @@
-// // import using require
-// import Shape from './shape.js';
-// // declare class
-// class Circle extends Shape{
-//     constructor(){
-//         super();
-//     }
-//     render(){
-//         return(<>
-//         <h1>{this.calculateArea}</h1></>);
-//     }
-// }
-// module.exports = Circle;
-// // export class using module.exports
 // import using require
-var msg = require('./shape.js');
+import Shape from './shape.js';
 // declare class
-
-
-// export class using module.exports
-
-module.exports= class Circle extends msg{
- constructor(...args){
-  super(...args); 
- }
-  calculateArea(){
-    console.log("2*pi*r"); 
-   return 10;
-  }
+class Circle extends Shape{
+    constructor(){
+        super();
+    }
+ calculateArea(){return " this is circle";}
+    render(){
+        return({calculateArea});
+    }
 }
+module.exports = Circle;
+// export class using module.exports
+// // import using require
+// var msg = require('./shape.js');
+// // declare class
+
+
+// // export class using module.exports
+
+// module.exports= class Circle extends msg{
+//  constructor(...args){
+//   super(...args); 
+//  }
+//   calculateArea(){
+//     console.log("2*pi*r"); 
+//    return 10;
+//   }
+// }
